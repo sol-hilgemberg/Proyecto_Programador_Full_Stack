f1d5072103676781f2d7ad3121d363ead29475b3
GustavoNievas <gustavonievasjofre8@gmail.com>
2023-05-06 20:44:17 -0300

    Se sube lo trabajado hasta el momento en Angular

---
+++ Migracion a angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+ import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrincipalComponent } from './home/principal/principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
